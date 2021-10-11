1c50622bc0c7bcb03833318a6544dd414bdce1e7
Hiren Gohil <hiren1640@gmail.com>
2021-10-11 20:05:33 +0100

    this is my first java file

---
+++ FirstName.java
@@ -0,0 +1,17 @@
+package personalDetails;
+
+public class FirstName {
+    //Main Method to execute the program
+
+    public static void main(String[] args) {
+
+
+        System.out.print("Hiren Gohil ");
+        System.out.println("Velji");
+        System.out.print("Age-28");
+        System.out.println("  DOB- 02/04/1993");
+        System.out.println("Address: LE27JD");
+        System.out.println("         Leicester");
+
+    }
+}
---
+++ Homework.java
@@ -0,0 +1,27 @@
+package personalDetails;
+
+public class Homework {
+
+    public static void main(String[] args) {
+        // Main method to execute program
+
+        System.out.println("What is Software?");
+        System.out.print("What is Software testing?");
+        System.out.println(" Types of software testing?");
+        System.out.println("What is manual testing?");
+        System.out.println("How to you find Manual testing?");
+        System.out.println("What is SDLC?");
+        System.out.println("What is STLC?");
+        System.out.println("Why are there bugs in Software?");
+        System.out.println("");
+        System.out.println("What are the testing techniques?");
+        System.out.println("What is severity?");
+        System.out.println("What is priority?");
+        System.out.println("Why is it important to test a software?");
+        
+
+    }
+
+
+}
+
